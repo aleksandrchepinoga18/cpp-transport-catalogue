58c9b5f9dcebe368b690fc7895e43bfdfe696c90
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-06-29 11:16:27 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,24 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+ 
+namespace transport_catalogue {
+namespace detail {  
+namespace stop {
+    Stop parse_stop(const std::string& str);
+    
+}//end namespace stop
+    
+namespace distance {
+    std::vector<Distance> parse_distances(const std::string& str, TransportCatalogue& catalogue);
+    
+}//end namespace distance
+    
+namespace bus {
+    Bus parse_bus(std::string_view str, TransportCatalogue& catalogue);
+    
+}//end namespace bus
+ 
+void fill_catalogue(std::istream& input, TransportCatalogue& catalogue);
+ 
+}//end namespace detail 
+}//end namespace transport_catalogue
