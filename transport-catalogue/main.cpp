b065b00688705031e643b8a6bb62163ff49f0b40
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-06 17:54:24 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,13 +1,12 @@
 #include "transport_catalogue.h"
 #include "input_reader.h"
 #include "stat_reader.h"
- 
+
+#include <iostream>
+
 using namespace std;
-using namespace transport_catalogue;
-using namespace transport_catalogue::detail;
- 
-int main() {
-    TransportCatalogue catalogue;
-    fill_catalogue(std::cin, catalogue);
-    parse_and_execute_queries(std::cin, catalogue);
+
+int main() {     
+catalog::TransportCatalogue catalogue;
+catalog::input_utils::TransportRequest(std::cin); 
 } 
