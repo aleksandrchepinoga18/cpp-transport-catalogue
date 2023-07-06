63233eb83822b2d3575370218742659fd2219e66
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-06 17:56:23 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,40 +1,13 @@
 #pragma once
+#include <string>
+
 #include "transport_catalogue.h"
-#include <algorithm>
-#include <vector>
- 
-namespace transport_catalogue {
-namespace detail {
-namespace bus {
+
+namespace catalog::output_utils {
     
-struct BusQueryResult{
-    std::string_view name;
-    bool not_found;
-    size_t stops_on_route;
-    size_t unique_stops;
-    size_t route_length;
-    double curvature;
-};
-    
-void show_terminal(BusQueryResult bus_info, std::ostream& output);
-BusQueryResult query(TransportCatalogue& catalogue, std::string_view str);
-    
-}//end namespace bus
-    
-namespace stop {
-struct StopQueryResult{
-    std::string_view name;
-    bool not_found;
-    std::vector <std::string_view> buses_name;
-};
-    
-void show_terminal(const StopQueryResult& stop_info, std::ostream& output);
-StopQueryResult query(TransportCatalogue& catalogue, std::string_view stop_name);
-    
-}//end namespace stop
-    
-void execute_query(TransportCatalogue& catalogue, std::string_view str);
-void parse_and_execute_queries(std::istream& input, TransportCatalogue& catalogue);
-    
-}//end namespace detail
-}//end namespace transport_catalogue
+std::string_view ReqBusStat(std::string_view text);
+std::string_view BusSearchToReq(std::string_view text);
+void PrintBusStop(std::ostream& os, std::string_view stop_name,
+                                  const std::set<std::string_view>* buses);
+
+}  // namespace catalog::output_utils
