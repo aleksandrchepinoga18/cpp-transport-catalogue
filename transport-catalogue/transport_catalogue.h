833bc253f0910864ccab57bf420adec835f1f29b
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-06 17:58:05 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,81 +1,94 @@
 #pragma once
+
 #include <deque>
+#include <list>
+#include <optional>
+#include <set>
 #include <string>
-#include <vector>
- 
-#include <iomanip>
-#include <iostream>
- 
-#include <unordered_set>
+#include <string_view>
 #include <unordered_map>
- 
+#include <unordered_set>
+#include <vector>
+
 #include "geo.h"
- 
-namespace transport_catalogue {   
-    
-struct Bus;
-    
-struct Stop {    
+
+using ToStopsRoutes = std::vector<std::pair<std::string_view, int>>;
+
+namespace catalog {
+
+enum class RouteType { CIRCLE, TWO_DIRECTIONAL };
+
+struct Bus {
+    std::string number;
+    RouteType type;
+    std::vector<std::string_view> stop_names;
+    std::set<std::string_view> unique_stops;
+
+ size_t GetStopsCount() const;
+};
+
+struct Stop {
     std::string name;
-    double latitude;
-    double longitude;
-    
-    std::vector<Bus*> buses;
+    geo::Coordinates point;
+
+    size_t Hash() const {
+    size_t hash_value = std::hash<std::string>{}(name); //значение хеша имени остановки 
+    hash_value += even_value * std::hash<double>{}(point.lng); // добавляем хеш долготы 
+    hash_value += even_value * even_value * std::hash<double>{}(point.lat); // хеш широты 
+    return hash_value;
+}
+
+private:
+    static const size_t even_value{37};
 };
- 
-struct Bus { 
-    std::string name;
-    std::vector<Stop*> stops;
-    size_t route_length;
+
+using PointStops = std::pair<const Stop*, const Stop*>;
+
+struct BusStatistics {
+    std::string_view number;
+    size_t stops_count{0u};
+    size_t unique_stops_count{0u};
+    int rout_length{0};
+    double curvature{0.};
 };
- 
-struct Distance {    
-    const Stop* A;
-    const Stop* B;
-    int distance;
-};
- 
-struct DistanceHasher {
-    std::hash<const void*> hasher;
-    
-    std::size_t operator()(const std::pair<const Stop*, const Stop*> pair_stops) const noexcept {
-        auto hash_1 = static_cast<const void*>(pair_stops.first);
-        auto hash_2 = static_cast<const void*>(pair_stops.second);
-        return hasher(hash_1) * 17 + hasher(hash_2);
-    }  
-};
-    
-typedef  std::unordered_map<std::string_view, Stop*> StopMap;
-typedef  std::unordered_map<std::string_view, Bus*> BusMap;
-typedef  std::unordered_map<std::pair<const Stop*, const  Stop*>, int, DistanceHasher> DistanceMap;
- 
+
+std::ostream& operator<<(std::ostream& os, const BusStatistics& statistics);
+
 class TransportCatalogue {
 public:  
-    
-    void add_bus(Bus&& bus);
-    void add_stop(Stop&& stop);
-    void add_distance(const std::vector<Distance>& distances);
-    
-    Bus* get_bus(std::string_view bus_name);
-    Stop* get_stop(std::string_view stop_name);
- 
-    std::unordered_set<const Bus*> stop_get_uniq_buses(Stop* stop);    
-    std::unordered_set<const Stop*> get_uniq_stops(Bus* bus);
-    double get_length(Bus* bus);
-    
-    size_t get_distance_stop(const Stop* start, const Stop* finish);
-    size_t get_distance_to_bus(Bus* bus);
-    
-private:
-    
-    std::deque<Stop> stops;
-    StopMap stopname_to_stop;
-    
-    std::deque<Bus> buses;
-    BusMap busname_to_bus;
-    
-    DistanceMap distance_to_stop;
-    
+    TransportCatalogue() = default;
+
+public:   
+    void AddStop(Stop stop);
+    void AddBus(Bus bus);
+    void AddDistance(std::string_view stop_from, std::string_view stop_to, int distance);
+
+     std::optional<BusStatistics> BusStat(std::string_view bus_number) const;
+       const std::set<std::string_view>* GetBusStop(std::string_view stop_name) const;
+
+private:   
+    struct PointStopsHash {
+        size_t operator()(const PointStops& pair) const {
+            return pair.first->Hash() + prime_number * pair.second->Hash();
+        }
+
+    private:
+        static const size_t prime_number{31};
+    };
+
+private:  
+    int AllRouteLen(const Bus* bus_info) const;
+    double GeoLenCal(const Bus* bus_info) const;
+
+private:   
+    std::deque<Stop> stops_storage_;
+    std::unordered_map<std::string_view, const Stop*> stops_;
+
+    std::deque<Bus> buses_storage_;
+    std::unordered_map<std::string_view, const Bus*> buses_;
+
+    std::unordered_map<std::string_view, std::set<std::string_view>> buses_through_stop_;
+    std::unordered_map<PointStops, int, PointStopsHash> distances_between_stops_;
 };
-    
-}//end namespace transport_catalogue
+
+}  // namespace catalog
