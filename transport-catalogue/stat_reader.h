bf89919719de92da18f54ff0d50f23a0d94e5166
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-09 00:45:12 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -7,7 +7,9 @@ namespace catalog::output_utils {
     
 std::string_view ReqBusStat(std::string_view text);
 std::string_view BusSearchToReq(std::string_view text);
-void PrintBusStop(std::ostream& os, std::string_view stop_name,
-                                  const std::set<std::string_view>* buses);
-
+void PrintBusStop(std::ostream& os, std::string_view stop_name,const std::set<std::string_view>* buses);
+    
+std::ostream& operator<<(std::ostream& os, const BusStatistics& bus_info);
+void ParseTransportRequest(std::istream& input_stream, const TransportCatalogue& catalogue);
+ 
 }  // namespace catalog::output_utils
