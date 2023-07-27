920cb346ca340f896a2735e27d83e0deb5072db8
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-28 00:05:26 +0300

    Create request_handler.h

---
+++ transport-catalogue/request_handler.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*
+* Описание: модуль обработки запросов.
+ * Действует как Фасад, упрощающий взаимодействие с транспортным каталогом
+ */
+
+#include "json_reader.h"
+
+namespace request {
+
+void ProcessTransportCatalogueQuery(std::istream& input, std::ostream& output);
+
+}  // namespace request
