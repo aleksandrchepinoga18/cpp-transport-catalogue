90182cb13c0fc4b20b35d294cbbf3db04694218c
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-28 00:01:01 +0300

    Create json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/*
+* Описание: парсит данные JSON, созданные во время парсинга, и формирует массив
+ * ответов JSON
+ */
+
+#include "json.h"
+#include "map_renderer.h"
+#include "transport_catalogue.h"
+
+namespace request {
+
+catalogue::TransportCatalogue ProcessBaseRequest(const json::Array& requests);
+
+render::Visualization ParseVisualizationSettings(const json::Dict& settings);
+
+json::Node MakeStatResponse(const catalogue::TransportCatalogue& catalogue, const json::Array& requests,
+                            const render::Visualization& settings);
+
+}  // namespace request
