2f32991efca66c92db195843209056a0c8a8e805
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-07-31 18:10:19 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -11,19 +11,20 @@ void ProcessTransportCatalogueQuery(std::istream& input, std::ostream& output) {
     const auto input_json = json::Load(input).GetRoot();
 
     // Step 1. Формировать каталог на основе входных данных
-    TransportCatalogue catalogue;
     const auto& base_requests = input_json.AsMap().at("base_requests"s).AsArray();
-    auto transport_catalogue = ProcessBaseRequest(base_requests);
+    auto transport_catalogue = request::ProcessBaseRequest(base_requests);
 
     // Step 2. Разобрать настройки rendering 
     const auto& render_settings = input_json.AsMap().at("render_settings"s).AsMap();
-    const auto& visualization_settings = ParseVisualizationSettings(render_settings);
+    const auto& visualization_settings = request::ParseVisualizationSettings(render_settings);
 
     // Step 3. Форма ответа
     const auto& stat_requests = input_json.AsMap().at("stat_requests"s).AsArray();
-    auto response = MakeStatResponse(transport_catalogue, stat_requests, visualization_settings);
+    auto response = request::MakeStatResponse(transport_catalogue, stat_requests, visualization_settings);
 
     json::Print(json::Document{std::move(response)}, output);
+    
 }
 
 }  // namespace request
+
