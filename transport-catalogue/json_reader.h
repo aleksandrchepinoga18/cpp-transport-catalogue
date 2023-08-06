5002b83283e947bea995b04a81c8600b9947d431
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-08-06 15:57:49 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -9,13 +9,16 @@
 #include "map_renderer.h"
 #include "transport_catalogue.h"
 
+#include "domain.h"
+
 namespace request {
 
 catalogue::TransportCatalogue ProcessBaseRequest(const json::Array& requests);
-
+    
 render::Visualization ParseVisualizationSettings(const json::Dict& settings);
-
+    
 json::Node MakeStatResponse(const catalogue::TransportCatalogue& catalogue, const json::Array& requests,
                             const render::Visualization& settings);
+       
 
-}  // namespace request 
+}  // namespace request
